092d2bc3b523952d3d5bcaa2cd62581a21cfcf13
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-09-12 22:14:55 +0200

    start styling 💅

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,27 @@
 <script>
-  export let name;
+    export let name;
 </script>
 
 <style>
-  h1 {
-    font-size: 34px;
-    line-height: 40px;
-    font-weight: bold;
-  }
-  p {
-    color: var(--text-darker-colour);
-    font-weight: normal;
-    font-size: 20px;
-    line-height: 24px;
-  }
+    .header-wrap {
+        margin-left: 21px;
+        margin-top: calc(58px - 25px);
+        margin-bottom: 17px;
+    }
+    h1 {
+        font-size: 34px;
+        line-height: 40px;
+        font-weight: bold;
+    }
+    p {
+        color: var(--text-darker-colour);
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 24px;
+    }
 </style>
 
-<h1>Hi {name} 👋</h1>
-<p>How can I help you today?</p>
+<div class="header-wrap">
+    <h1>Hi {name} 👋</h1>
+    <p>How can I help you today?</p>
+</div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 export const iconPath = document.location.host.includes('localhost:6006') ? '' : 'icons/';
 export const imagePath = document.location.host.includes('localhost:6006') ? '' : 'images/';
+export const databaseUrl = 'https://imgine-db.glitch.me/data';
