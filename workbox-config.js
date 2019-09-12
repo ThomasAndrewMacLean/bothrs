771b5d7346efbea9c08288233733709ea51af027
Thomas MacLean <thomas.maclean@gmail.com>
2019-09-12 13:09:33 +0200

    also cache images 🖼

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     globDirectory: 'public/',
-    globPatterns: ['**/*.{css,js,png,svg,ico,jpeg,webmanifest,html}'],
+    globPatterns: ['**/*.{css,js,png,svg,ico,jpeg,jpg,webmanifest,html}'],
     swDest: 'public/worker.js',
     cleanupOutdatedCaches: true,
     skipWaiting: true,
