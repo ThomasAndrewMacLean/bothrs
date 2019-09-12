01f1cc7042d9e99b740ad1e255914ac38bc8e55b
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-09-12 22:44:45 +0200

    fix path bug 🐛

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
-export const iconPath = document.location.host.includes('localhost:6006') ? '' : 'icons/';
-export const imagePath = document.location.host.includes('localhost:6006') ? '' : 'images/';
+export const iconPath =
+    document.location.host.includes('localhost:5000') || document.location.host.includes('surge') ? 'icons/' : '';
+export const imagePath =
+    document.location.host.includes('localhost:5000') || document.location.host.includes('surge') ? 'images/' : '';
 export const databaseUrl = 'https://imgine-db.glitch.me/data';
