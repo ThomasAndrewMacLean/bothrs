bcf6e780a3e3d0754f2cd2a9f752847a89c4bb29
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-09-12 08:12:43 +0200

    testing icon/ image path 🖼

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,2 @@
-export const iconPath =
-    document.location.host.includes('localhost:6006') || document.location.host.includes('thomasandrewmaclean')
-        ? ''
-        : 'icons/';
-export const imagePath =
-    document.location.host.includes('localhost:6006') || document.location.host.includes('thomasandrewmaclean')
-        ? ''
-        : 'images/';
+export const iconPath = document.location.host.includes('localhost:6006') ? '' : 'icons/';
+export const imagePath = document.location.host.includes('localhost:6006') ? '' : 'images/';
