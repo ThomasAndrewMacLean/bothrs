23b949184a8b06a8d8b5cd873613904936db707f
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-09-16 20:45:40 +0200

    load icons for ntlify 🎃

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,15 @@
 export const iconPath =
-    document.location.host.includes('localhost:5000') || document.location.host.includes('surge') ? 'icons/' : '';
+    document.location.host.includes('localhost:5000') ||
+    document.location.host.includes('surge') ||
+    document.location.host.includes('netlify')
+        ? 'icons/'
+        : '';
 export const imagePath =
-    document.location.host.includes('localhost:5000') || document.location.host.includes('surge') ? 'images/' : '';
+    document.location.host.includes('localhost:5000') ||
+    document.location.host.includes('surge') ||
+    document.location.host.includes('netlify')
+        ? 'images/'
+        : '';
 export const databaseUrl = 'https://imgine-db.glitch.me/data';
 
 export const colourMap = {
